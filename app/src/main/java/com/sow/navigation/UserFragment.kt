43a3c84a0820e61a6073f28535ad0b957c052d6b
Renato Goncalves <renatoarg@gmail.com>
2020-03-21 19:20:06 -0300

    Initial commit

---
+++ app/src/main/java/com/sow/navigation/UserFragment.kt
@@ -0,0 +1,35 @@
+package com.sow.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_user.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class UserFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navigateUp.setOnClickListener{
+
+            findNavController().navigateUp()
+
+        }
+
+    }
+
+}
